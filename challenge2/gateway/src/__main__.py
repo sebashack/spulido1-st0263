1937ea330de422caa286f157776eb06ee25fd3ae
sebashack <sebastian0092@gmail.com>
2023-02-22 21:29:00 -0500

    Implement simple protocol for RPC service + dummy server/client

--- challenge2/gateway/src/__main__.py
+++ challenge2/gateway/src/__main__.py
@@ -2,9 +2,23 @@ import grpc
 import logging
 import sys
 
+import file_service_pb2
+import file_service_pb2_grpc
+
+
+def run():
+    with grpc.insecure_channel("localhost:50051") as channel:
+        stub = file_service_pb2_grpc.FileServiceStub(channel)
+        res0 = stub.ListFiles(file_service_pb2.ListFilesRequest(limit=100))
+        print("ListFiles -- received: \n" + res0.file_list)
+
+        res1 = stub.SearchFiles(file_service_pb2.SearchFilesRequest(limit=50, pattern="*.txt"))
+        print("SearchFiles -- received: \n" + res1.file_list)
+
 
 def main(argv):
-    print("Hello")
+    logging.basicConfig()
+    run()
 
 
 if __name__ == "__main__":
--- challenge2/micro1-grpc/src/__main__.py
+++ challenge2/micro1-grpc/src/__main__.py
@@ -1,11 +1,36 @@
+from concurrent import futures
 import asyncio
 import grpc
 import logging
 import sys
 
+import file_service_pb2
+import file_service_pb2_grpc
+
+class FileService(file_service_pb2_grpc.FileService):
+    def ListFiles(self, request, context):
+        print(f"Limit: {request.limit}")
+        return file_service_pb2.FilesReply(file_list=f"foo.txt\nbar.txt\n")
+
+    def SearchFiles(self, request, context):
+        print(f"Limit: {request.limit}")
+        print(f"Pattern: {request.pattern}")
+        return file_service_pb2.FilesReply(file_list=f"foo.txt\nbar.txt\n")
+
+
+def serve():
+    port = "50051"
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    file_service_pb2_grpc.add_FileServiceServicer_to_server(FileService(), server)
+    server.add_insecure_port("[::]:" + port)
+    server.start()
+    print("Server started, listening on " + port)
+    server.wait_for_termination()
+
 
 def main(argv):
-    print("Hello")
+    logging.basicConfig()
+    serve()
 
 
 if __name__ == "__main__":

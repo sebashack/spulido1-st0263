27b4f8d5a6f435169f39f58d319a2b9dac594c0c
sebashack <sebastian0092@gmail.com>
2023-02-26 13:24:41 -0500

    Setup config file for microservice 2

--- challenge2/micro2-mom/src/__main__.py
+++ challenge2/micro2-mom/src/__main__.py
@@ -1,15 +1,17 @@
+import argparse
 import json
 import logging
 import pathlib
 import pika
+import sys
 
 
 def list_files(dir_path, pattern="*", limit=None):
-    files = list(map(lambda p: str(p), pathlib.Path(dir_path).glob('**/' + pattern)))
+    files = list(map(lambda p: str(p), pathlib.Path(dir_path).glob("**/" + pattern)))
     if limit is None or limit < 1:
-        return {'found': len(files), 'files': files}
+        return {"found": len(files), "files": files}
     else:
-        return {'found': len(files), 'files': files[:limit]}
+        return {"found": len(files), "files": files[:limit]}
 
 
 def on_request(logger, dir_path, conn, channel, method, props, body):
@@ -17,43 +19,52 @@ def on_request(logger, dir_path, conn, channel, method, props, body):
     try:
         msg = json.loads(body)
         logger.info(f"Message with key '{method.routing_key} and type '{msg['type']}'")
-        if msg['type'] == 'search':
+        if msg["type"] == "search":
             result = list_files(dir_path, msg["pattern"], msg["limit"])
         else:
             result = list_files(dir_path, limit=msg["limit"])
-        result['req_id'] = msg['req_id']
+        result["req_id"] = msg["req_id"]
     except Exception as e:
         print(e)
-        logger.warning(f'Failed to process message')
-        result = {'error': 'Failed to process file request', 'req_id': msg['req_id']}
+        logger.warning(f"Failed to process message")
+        result = {"error": "Failed to process file request", "req_id": msg["req_id"]}
 
     channel.basic_publish(
-        exchange='fs-consumer', routing_key='msg-receive', body=json.dumps(result)
+        exchange="fs-consumer", routing_key="msg-receive", body=json.dumps(result)
     )
 
 
-def main():
-    dir_path = '/home/sebastian/univeristy/networking/spulido1-st0263/challenge2'
+def main(argv):
+    parser = argparse.ArgumentParser(description="File service API gateway")
+
+    parser.add_argument("config", type=str, help="Path to config file")
+
+    args = parser.parse_args(argv[1:])
+
+    config_path = args.config
+    conf = json.loads(open(config_path, "r").read())
 
     # MoM setup
-    credentials = pika.PlainCredentials('admin', 'secret')
+    credentials = pika.PlainCredentials(conf["rabbit_user"], conf["rabbit_password"])
     connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='127.0.0.1', port=5672, credentials=credentials)
+        pika.ConnectionParameters(
+            host=conf["rabbit_host"], port=conf["rabbit_port"], credentials=credentials
+        )
     )
 
     # Logging
-    FORMAT = '%(asctime)s -- %(message)s'
+    FORMAT = "%(asctime)s -- %(message)s"
     logging.basicConfig(format=FORMAT, level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S")
-    logger = logging.getLogger('mom-consumer-server')
+    logger = logging.getLogger("mom-consumer-server")
 
     def cb(ch, method, props, body):
-        on_request(logger, dir_path, connection, ch, method, props, body)
+        on_request(logger, conf["dir_path"], connection, ch, method, props, body)
 
     # Consume the producer queue
     channel = connection.channel()
-    channel.basic_consume(queue='fs-producer', auto_ack=True, on_message_callback=cb)
+    channel.basic_consume(queue="fs-producer", auto_ack=True, on_message_callback=cb)
     channel.start_consuming()
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)

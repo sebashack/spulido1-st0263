dba2528b2511c56cf4fa69f6e859e0b3ec19a1cc
sebashack <sebastian0092@gmail.com>
2023-03-13 18:24:18 -0500

    Setup working example for streaming

--- streaming/server/src/__main__.py
+++ streaming/server/src/__main__.py
@@ -1,73 +1,34 @@
-from concurrent import futures
-import argparse
-import file_service_pb2
-import file_service_pb2_grpc
-import glob
-import grpc
-import json
+import asyncio
 import logging
-import os
-import pathlib
-import sys
+
+import grpc
+from hellostreamingworld_pb2 import HelloReply
+from hellostreamingworld_pb2 import HelloRequest
+from hellostreamingworld_pb2_grpc import MultiGreeterServicer
+from hellostreamingworld_pb2_grpc import add_MultiGreeterServicer_to_server
+
+NUMBER_OF_REPLY = 10
 
 
-class FileService(file_service_pb2_grpc.FileService):
-    logger = None
-    dir_papth = None
+class Greeter(MultiGreeterServicer):
 
-    def __init__(self, dir_path):
-        FORMAT = "%(asctime)s -- %(message)s"
-        logging.basicConfig(
-            format=FORMAT, level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S"
-        )
-        self.logger = logging.getLogger("grpc-server")
-        self.dir_path = dir_path
-
-    def ListFiles(self, req, context):
-        self.logger.info(f"New list request, limit={req.limit}")
-        result = self.list_files(limit=req.limit)
-        return file_service_pb2.FilesReply(file_list=json.dumps(result))
-
-    def SearchFiles(self, req, context):
-        self.logger.info(
-            f"New search request, limit={req.limit}, pattern={req.pattern}"
-        )
-        result = self.list_files(req.pattern, req.limit)
-        return file_service_pb2.FilesReply(file_list=json.dumps(result))
-
-    def list_files(self, pattern="*", limit=None):
-        files = list(
-            map(lambda p: str(p), pathlib.Path(self.dir_path).glob("**/" + pattern))
-        )
-        if limit < 1 or limit is None:
-            return {"found": len(files), "files": files}
-        else:
-            return {"found": len(files), "files": files[:limit]}
+    async def sayHello(self, request: HelloRequest,
+                       context: grpc.aio.ServicerContext) -> HelloReply:
+        logging.info("Serving sayHello request %s", request)
+        for i in range(NUMBER_OF_REPLY):
+            yield HelloReply(message=f"Hello number {i}, {request.name}!")
 
 
-def serve(port, dir_path, workers):
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=workers))
-    file_service_pb2_grpc.add_FileServiceServicer_to_server(
-        FileService(dir_path), server
-    )
-    server.add_insecure_port(f"[::]:{port}")
-    server.start()
-    print(f"Server started, listening on {port}")
-    server.wait_for_termination()
-
-
-def main(argv):
-    parser = argparse.ArgumentParser(description="File service API gateway")
-
-    parser.add_argument("config", type=str, help="Path to config file")
-
-    args = parser.parse_args(argv[1:])
-
-    config_path = args.config
-    conf = json.loads(open(config_path, "r").read())
-
-    serve(conf['port'], conf['dir_path'], conf['max_workers'])
+async def serve() -> None:
+    server = grpc.aio.server()
+    add_MultiGreeterServicer_to_server(Greeter(), server)
+    listen_addr = "[::]:50051"
+    server.add_insecure_port(listen_addr)
+    logging.info("Starting server on %s", listen_addr)
+    await server.start()
+    await server.wait_for_termination()
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(serve())

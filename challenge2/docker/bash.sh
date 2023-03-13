bfb2e79e8ff589bddba862c7d34428b7c71bc127
sebashack <sebastian0092@gmail.com>
2023-03-13 18:15:37 -0500

    Clean up previous project

--- challenge2/docker/bash.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker exec -it devel_rabbitmq bash
--- challenge2/test-scripts/search0.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while :
-do
-  date +%s
-  curl --stderr - "http://localhost:8001/files/search?pattern=*.txt" | grep -o '\"provider\"......'
-  echo "REQ_STATUS=$?"
-  sleep 0.1
-done
